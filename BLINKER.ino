e2965c2bcb2c440d72a6d49cd560bfc9c05b3794
Samiran Medhi <145327349+coder-441@users.noreply.github.com>
2024-10-01 11:03:20 +0530

    Add files via upload

---
+++ BLINKER.ino
@@ -0,0 +1,48 @@
+int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+}
